4b0ad5336cde7ee884656c8429ff9455decb4354
llx <lololx0131@gmail.com>
2024-11-18 01:24:51 +0800

    Add Cat class with attributes and methods
    Introduced a Cat class in main.py with attributes for the cat’s name and sex. The class includes methods to print and set the cat’s name, as well as a string representation method. Additionally, updated the main section to create a Cat instance and demonstrate these methods.

--- main.py
+++ main.py
@@ -14,9 +14,33 @@ def test(a, b):
     c = a + b
     return c
 
+class Cat:
+    def __init__(self, name_, sex_):
+        self.name = name_
+        self.sex = sex_
+
+    def __str__(self):
+        return self.name+",,,"+self.sex
+
+
+    def print_name(self):
+        print(self.name)
+
+    def set_name(self,name):
+        self.name = name
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     c = test(1, 2)
     print(c)
+
+
+    cat = Cat ("Tom", "Male")
+    print(cat)
+    print(cat.sex)
+
+
+    cat.print_name()
+    cat.set_name("Jerry")
+    cat.print_name()
